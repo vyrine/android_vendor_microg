b7a70ac40a18dce63be8a9842ab22cce10bc0e90
flame <fl4m3@pm.me>
2023-05-09 05:08:13 +0800

    Initial fork commit

--- products/gms.mk
+++ products/gms.mk
@@ -1,9 +1,6 @@
 PRODUCT_PACKAGES += \
-	GmsCore \
-	GsfProxy \
-	FakeStore \
-	IchnaeaNlpBackend \
-	NominatimGeocoderBackend \
-	FDroid \
-	FDroidPrivilegedExtension \
-	additional_repos.xml
+    FakeStore \
+    GmsCore \
+    GsfProxy \
+    IchnaeaNlpBackend \
+    NominatimGeocoderBackend
